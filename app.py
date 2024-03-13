0898897b0fbcfc139726d6e2fd3784ce599c43ed
Oscar Miranda Puentes <110064371+OscarMirandaPuentes@users.noreply.github.com>
2024-03-13 09:55:10 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,24 +2,23 @@ import requests
 import boto3
 from datetime import datetime
 
-def download_and_store_pages(event, context):
-    # Descargar las 5 primeras páginas
-    for i in range(1, 6):
-        url = f"https://casas.mitula.com.co/pagina-{i}"
+def f(event, context):
+    s3 = boto3.client('s3')
+    bucket_name = 'buckets-raws'
+    
+    for page_number in range(1, 6):
+        if(page_number==1):
+            url = f'https://casas.mitula.com.co/casas/pereira/'
+        else:
+            url = f'https://casas.mitula.com.co/casas/pereira/{page_number}'
         response = requests.get(url)
         
-        # Almacenar en S3
-        bucket_name = 'parcial-def'
-        date_string = datetime.now().strftime("%Y-%m-%d")
-        key = f'casas/contenido-pag-{i}-{date_string}.html'
-        
-        s3 = boto3.client('s3')
-        s3.put_object(
-            Body=response.content,
-            Bucket=bucket_name,
-            Key=key
-        )
-
-def f(event, context):
-    # Punto de entrada para Zappa
-    download_and_store_pages(event, context)
+        if response.status_code == 200:
+            current_date = datetime.now().strftime('%Y-%m-%d')
+            file_key = f'casas/contenido-pag-{page_number}-{current_date}.html'
+            s3.put_object(Body=response.content, Bucket=bucket_name, Key=file_key)
+    
+    return {
+        'statusCode': 200,
+        'body': 'Páginas descargadas y guardadas en S3 correctamente.'
+    }
